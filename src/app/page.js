2153a44a1dc40e069b3e4aa5be74a470b8259edf
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-08-13 23:59:15 +0600

    simple navigation and linking concept

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
-import Image from 'next/image'
+'use client'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router=useRouter();
+  
+  const navigate=(route)=>{
+    router.push(route);
+  }
   return (
     <main>
      <h2>Home</h2>
+     <Link href='/about'>About Page</Link>
+     <br />
+     <button onClick={()=>navigate('/contact')}>Contact page</button>
     </main>
   )
 }
