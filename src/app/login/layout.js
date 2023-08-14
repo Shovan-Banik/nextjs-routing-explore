f9521d9ca60317f50bd212b7eaf11e9eb943a995
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-08-14 12:12:55 +0600

    conditional layout explore

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,15 +1,22 @@
+'use client'
 import Link from 'next/link';
 import React from 'react';
-import './login.css'
+import './login.css';
+import { usePathname } from 'next/navigation';
 
 const Layout = ({children}) => {
+    const pathName=usePathname();
+    console.log(pathName);
     return (
         <div>
-            <ul className='login-nav'>
+            {
+                pathName !== "/login/loginTeacher" ?
+                <ul className='login-nav'>
                 <li><Link href='/login'>Main login page</Link></li>
                 <li><Link href='/login/loginStudent'>Student login page</Link></li>
                 <li><Link href='/login/loginTeacher'>Teacher login page</Link></li>
-            </ul>
+            </ul> : <li><Link href='/login'>Main Login</Link></li>
+            }
             {children}
         </div>
     );
